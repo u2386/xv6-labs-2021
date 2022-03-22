7cc2c7ecf87584c0ed721e481c234dfc2078aebe
hugo <hugo.cavan2386@gmail.com>
2022-03-22 13:10:27 +0800

    feat: backtrace
    fix backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -117,6 +117,8 @@ printf(char *fmt, ...)
 void
 panic(char *s)
 {
+  backtrace();
+
   pr.locking = 0;
   printf("panic: ");
   printf(s);
@@ -131,4 +133,16 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void backtrace(void)
+{
+  uint64 fp, v;
+  fp = r_fp();
+  printf("backtrace:\n");
+  while (fp < PGROUNDUP(fp) && fp > PGROUNDDOWN(fp)) {
+    v = *(uint64*)(fp-8);
+    printf("%p\n", v);
+    fp = *(uint64*)(fp-16);
+  }
 }
